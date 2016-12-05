d17bcacb26cdb44bf26e27038ead1e8400de97f9
dacodemaniak <jean-luc.aubert@web-projet.com>
2016-12-05 16:40:50 +0100

    Mise à jour définitive
    Change-Id: Idd4c465f78c4cdf358f5f29e816ad7946a7a05d2
    Change-Id: 1.1

--- Objet/Initiation/Classes/Personne/Personne.class.php
+++ Objet/Initiation/Classes/Personne/Personne.class.php
@@ -7,22 +7,56 @@
 **/
 class Personne {
 	/**
-	 * @var public string $nom
+	 * @var private string $nom
 	 *	Stocke le nom de la personne physique qui sera instanciée
 	 **/
-	public $nom;
+	private $nom;
 
 	/**
-	 * @var public string $prenom
+	 * @var private string $prenom
 	 *	Stocke le prénom de la personne physique qui sera instanciée
 	 **/
-	public $prenom;
+	private $prenom;
 	
 	/**
 	 * Définit et stocke l'âge de la personne
 	 * @var int
 	 */
 	private $age;
+	
+	private $anneeNaissance;
+	
+	/**
+	 * public void __construct(string $nom, string $prenom)
+	 *	@param string $nom : Nom de la personne
+	 *	@param string $prenom : Prénom de la personne
+	 **/
+	public function __construct($nom, $prenom){
+		$this->nom = $nom;
+		$this->prenom = $prenom;
+	}
+
+	/**
+	 * public void __destruct(void)
+	 **/
+	public function __destruct(){
+		echo "Vous nous quittez déjà ? " . $this->prenom . " " . $this->nom . "<br />";
+	}
+
+	/**
+	 * public string __toString(void)
+	 *	Méthode automatiquement appelée par PHP lorsqu'on fait echo $objet;
+	 **/
+	public function __toString(){
+		$dump = "<pre><code>";
+		$dump .= "Attribut public nom => " . $this->nom . "<br />\n";
+		$dump .= "Attribut public prenom => " . $this->prenom . "<br />\n";
+		$dump .= "Attribut prive age => " . $this->age . "<br />\n";
+		$dump .= "Attribut prive anneeNaissance => " . $this->anneeNaissance . "<br />\n";
+		$dump .= "</code></pre>";
+	
+		return $dump;
+	}
 	
 	/**
 	 * public void setAge(int $age)
@@ -32,6 +66,7 @@ class Personne {
 	public function setAge($age){
 		if(is_numeric($age)){
 			$this->age = $age;
+			$this->setAnneeNaissance();
 		}
 	}
 
@@ -43,6 +78,7 @@ class Personne {
 	public function definitAge($age){
 		if(is_numeric($age) && $age > 0){
 			$this->age = $age;
+			$this->setAnneeNaissance();
 			return true;
 		}
 		return false;
@@ -67,9 +103,56 @@ class Personne {
 		if(!is_null($age)){
 			if(is_numeric($age)){
 				$this->age = $age;
+				$this->setAnneeNaissance();
 				return $this;
 			}
 		}
 		return $this->age;
+	}
+	
+	/**
+	 * public string getNom(void)
+	 *	@return string Valeur de l'attribut privé $this->nom définit dans le constructeur
+	 **/
+	public function getNom(){
+		return $this->nom;
+	}
+	
+	/**
+	 * public string getPrenom(void)
+	 *	@return string Valeur de l'attribut privé $this->prenom définit dnas la constructeur
+	 **/
+	public function getPrenom(){
+		return $this->prenom;
+	}
+	
+	/**
+	 * public string getNomComplet(void)
+	 *	@return string : Concaténation de l'attribut privé $this->prenom et de l'attribut privé $this->nom
+	 **/
+	public function getNomComplet(){
+		return $this->prenom . " " . $this->nom;
+	}
+
+	/**
+	 * public string getAnneeNaissance(void)
+	 *	@return string : année de naissance ou message d'erreur si le calcul n'a pas pu être fait
+	 **/
+	public function getAnneeNaissance(){
+		if(!is_null($this->anneeNaissance)){
+			return $this->anneeNaissance;
+		}
+		return "L'année de naissance n'a pas pu être calculée, vérifiez les appels dans votre code !";
+	}
+	
+	/**
+	 * private void setAnneeNaissance(void)
+	 *	Calcule l'année de naissance à partir de l'âge qui a été défini, et de l'année courante
+	 **/
+	private function setAnneeNaissance(){
+		$anneeCourante = 0;
+		
+		$anneeCourante = date("Y"); // La fonction interne PHP date() retourne la date courante
+		$this->anneeNaissance = $anneeCourante - $this->age;
 	}
 }
--- Objet/Initiation/instanciation.php
+++ Objet/Initiation/instanciation.php
@@ -11,31 +11,39 @@ require("Classes/Personne/Personne.class.php");
 /**
 * On peut donc instancier un premier objet de la classe Personne
 **/
-$formateur = new Personne;
-$formateur->nom = "Aubert"; // Affecte Aubert à la propriété publique nom de la classe Personne
-$formateur->prenom = "Jean-Luc"; // Affecte Jean-Luc à la propriété publique prenom de la classe Personne
-$formateur->setAge(48);
+$formateur = new Personne("Aubert", "Jean-Luc");
+//$formateur->nom = "Aubert"; // Affecte Aubert à la propriété publique nom de la classe Personne
+//$formateur->prenom = "Jean-Luc"; // Affecte Jean-Luc à la propriété publique prenom de la classe Personne
+$formateur->age(48);
+echo $formateur;
+echo $formateur->getAnneeNaissance();
 
 /**
 * On instancie un second objet de la classe Personne
 **/
-$stagiaire = new Personne;
-$stagiaire->nom = "Martin";
-$stagiaire->prenom = "Arthur";
+$stagiaire = new Personne("Martin", "Arthur");
+//$stagiaire->nom = "Martin";
+//$stagiaire->prenom = "Arthur";
 $stagiaire->setAge("Je suis âgé de 25 ans");
+echo $stagiaire;
+$stagiaire->anneeNaissance = "2012";
+echo $stagiaire->getAnneeNaissance();
 
-$autreStagiaire = new Personne;
-$autreStagiaire->nom = "Talut";
-$autreStagiaire->prenom = "Jean";
+$autreStagiaire = new Personne("Talut", "Jean");
+//$autreStagiaire->nom = "Talut";
+//$autreStagiaire->prenom = "Jean";
 if($autreStagiaire->definitAge("35")){
 	echo "C'est bon, l'âge a bien été défini.<br />";
 } else {
 	echo "Il y a un problème de définition de l'âge<br />";
 }
-
+echo $autreStagiaire; // ON voit que l'année de naissance sera incorrect, le calcul ayant été fait avant la définition de l'âge
+echo $autreStagiaire->getAnneeNaissance();
 
 /**
- * On peut afficher un Hello des deux instances (objets)
- **/
-echo "Hello " . $formateur->prenom . " " . $formateur->nom . "<br /> Mon âge : " . $formateur->getAge() . "<br />";
-echo "Hello aussi à toi : " . $stagiaire->prenom . " " . $stagiaire->nom . "<br />Mon âge : " . $stagiaire->getAge() . "<br />";
+* On peut afficher un Hello des deux instances (objets)
+**/
+echo "Hello " . $formateur->getPrenom() . " " . $formateur->getNom() . "<br /> Mon âge : " . $formateur->age() . "<br />";
+echo "Hello aussi à toi : " . $stagiaire->getPrenom() . " " . $stagiaire->getNom() . "<br />Mon âge : " . $stagiaire->getAge() . "<br />";
+echo "Hello le dernier objet : " . $autreStagiaire->getNomComplet() . "<br />";
+echo $formateur;
