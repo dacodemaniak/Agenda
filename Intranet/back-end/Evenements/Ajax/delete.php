d84b0169d7f2558282fcdff78ae32bdeb1895b11
dacodemaniak <jean-luc.aubert@web-projet.com>
2017-01-12 12:50:24 +0100

    Intégration appLoader dans les scripts Ajax

--- Intranet/back-end/Evenements/Ajax/delete.php
+++ Intranet/back-end/Evenements/Ajax/delete.php
@@ -4,11 +4,8 @@
  **/
 $id = $_POST["id"];
 
-// On devrait procéder au traitement lui-même...
-if(file_exists("../modele/evenements.class.php"))
-	require_once("../../../Classes/App/modele/evenements.class.php");
-else 
-	require_once("../../modele/evenements.class.php");
+require_once(dirname(__FILE__) . "/../../../appLoader.class.php");
+$appLoader = new appLoader();
 
 $evenement = new evenements();
 $evenement->delete($id);
